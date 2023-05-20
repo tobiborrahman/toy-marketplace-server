40fb8d50aa60fff0de3f604a62d8e84d5c1d7c21
TobiborRahman <r.tobibor16@gmail.com>
2023-05-20 18:46:44 +0600

    database collection created

--- index.js
+++ index.js
@@ -1,10 +1,100 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ytasiev.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		// await client.connect();
+
+		const toyCollection = client.db('toyMarketplace').collection('toys');
+		const cricketCollection = client
+			.db('toyMarketplace')
+			.collection('cricket');
+
+		const footballCollection = client
+			.db('toyMarketplace')
+			.collection('football');
+
+		const volleyballCollection = client
+			.db('toyMarketplace')
+			.collection('volleyball');
+
+		app.get('/toys', async (req, res) => {
+			const result = await toyCollection.find().toArray();
+			res.send(result);
+		});
+
+		app.post('/toys', async (req, res) => {
+			const newToy = req.body;
+			console.log(newToy);
+			const result = await toyCollection.insertOne(newToy);
+			res.send(result);
+		});
+
+		app.get('/football', async (req, res) => {
+			const filter = await footballCollection.find().toArray();
+			res.send(filter);
+		});
+
+		app.post('/football', async (req, res) => {
+			const newCricket = req.body;
+			console.log(newCricket);
+			const result = await footballCollection.insertOne(newCricket);
+			res.send(result);
+		});
+
+		app.get('/cricket', async (req, res) => {
+			const filter = await cricketCollection.find().toArray();
+			res.send(filter);
+		});
+
+		app.post('/cricket', async (req, res) => {
+			const newCricket = req.body;
+			console.log(newCricket);
+			const result = await cricketCollection.insertOne(newCricket);
+			res.send(result);
+		});
+
+		app.get('/volleyball', async (req, res) => {
+			const filter = await volleyballCollection.find().toArray();
+			res.send(filter);
+		});
+
+		app.post('/volleyball', async (req, res) => {
+			const newCricket = req.body;
+			console.log(newCricket);
+			const result = await volleyballCollection.insertOne(newCricket);
+			res.send(result);
+		});
+		// Send a ping to confirm a successful connection
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	res.send('Toy marketplace is running');
